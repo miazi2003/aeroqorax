189157ac60a2fcd69bf7ca3551af6a799ef349bd
Yeasin Miazi <yeasinmiazi1997@gmail.com>
2026-08-09 12:30:06 +0600

    feat: normalize ADS-B aircraft data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import axios from "axios";
+import type { Aircraft, RawAircraft } from "./types.js";
 const app = express();
 
 app.use(cors());
@@ -17,9 +18,30 @@ app.get("/", (req, res) => {
 
 app.get("/api/aircraft" , async(req, res)=>{
 try{
-const {data} = await axios.get("https://api.adsb.lol/v2/lat/23.8103/lon/90.4125/dist/110");
-console.log( "airplane data" , data)
-res.json(data)
+const {data} = await axios.get("https://api.adsb.lol/v2/lat/23.8103/lon/90.4125/dist/250");
+
+const aircraftData = data.ac;
+const normalizeData : Aircraft[] = aircraftData.filter((ac : RawAircraft)=>{
+  return ac.lat !== undefined && ac.lon !== undefined
+}).map(
+  (ac : RawAircraft)=>({
+  id: ac.hex,
+  callsign: ac.flight?.trim(),
+  registration: ac.r,
+  aircraftType: ac.t,
+
+  latitude: ac.lat,
+  longitude: ac.lon,
+
+  altitude: ac.alt_baro,
+  groundSpeed: ac.gs,
+  heading: ac.track,
+
+  verticalRate: ac.baro_rate,
+  squawk: ac.squawk,
+  emergency: ac.emergency
+}))
+res.json(normalizeData)
 }catch(error){
   console.error("Failed to fetch aircraft:", error);
 
